29c732f50a3afee10dbd221b1272b176759cf810
Sameer Ojha <146799637+Sameer172003@users.noreply.github.com>
2025-07-07 12:09:56 +0530

    added

--- tut11.py
+++ tut11.py
@@ -5,18 +5,18 @@ import pandas as pd
 var1=pd.DataFrame({"A":[1,2,3,4],"B":[5,6,7,8]})
 var2=pd.DataFrame({"C":[10,20],"D":[11,22]})
 
-# print(var1.join(var2))
+print(var1.join(var2))
 
-# print()
+print()
 
-# print(var2.join(var1))
+print(var2.join(var1))
 
-# print()
+print()
 
-# print(var2.join(var1,how="left"))
-# print(var2.join(var1,how="inner"))
-# print(var2.join(var1,how="right"))
-# print(var2.join(var1,how="outer"))
+print(var2.join(var1,how="left"))
+print(var2.join(var1,how="inner"))
+print(var2.join(var1,how="right"))
+print(var2.join(var1,how="outer"))
 
 v1=pd.DataFrame({"A":[1,2,3,4],"B":[5,6,7,8]})
 v2=pd.DataFrame({"C":[10,20],"B":[11,22]})
