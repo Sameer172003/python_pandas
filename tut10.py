1082ee5ada3ac9a65322cac51e95249d3dc07186
Sameer Ojha <146799637+Sameer172003@users.noreply.github.com>
2025-07-07 11:40:32 +0530

    added

--- tut10.py
+++ tut10.py
@@ -11,12 +11,12 @@ var1=pd.DataFrame({"A":[1,2,3,4],"B":[5,6,7,8]})
 
 var2=pd.DataFrame({"A":[1,2,3,5],"C":[13,14,15,16]})
 
-# print(pd.merge(var1,var2,how="inner")) 
-# print(pd.merge(var1,var2,how="left")) 
-# print(pd.merge(var1,var2,how="right")) 
-# print(pd.merge(var1,var2,how="outer")) 
+print(pd.merge(var1,var2,how="inner")) 
+print(pd.merge(var1,var2,how="left")) 
+print(pd.merge(var1,var2,how="right")) 
+print(pd.merge(var1,var2,how="outer")) 
 
-# print(pd.merge(var1,var2,left_index=True,right_index=True))
+print(pd.merge(var1,var2,left_index=True,right_index=True))
 
 s1=pd.Series([1,2,3,4])
 s2=pd.Series([5,6,7,8])
